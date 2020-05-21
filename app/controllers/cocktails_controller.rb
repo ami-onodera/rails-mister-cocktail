b0f1ce44ad91ce5c793cc1ed9eda512a11eb11d1
Ami Onodera <onodera.camila@gmail.com>
2020-05-21 15:12:13 +0200

    models and routes raking green

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show; end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail), notice: 'The cocktail was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail, notice: 'The cocktail was successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+    resources :reviews, only: %i[new create destroy]
+  end
+  resources :doses, only: %i[destroy]
 end
